1cfd05d4397a7205107f5a97a5dba0543721dea3
Rafael da Silva <crafards@gmail.com>
2013-06-05 17:21:38 -0300

    comitando com build.xml

---
+++ lab1/src/ufcg/si1/MinhaClasseTest.java
@@ -0,0 +1,26 @@
+package ufcg.si1;
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MinhaClasseTest {
+	
+	private MinhaClasse obj;
+	
+	@Before
+	public void inicializaClasse(){
+		obj = new MinhaClasse();
+	}
+	
+	@Test
+	public void testMensagemXPTO() {
+		
+		String expected = "Este eh o primeiro lab de SI1";
+		String actual = obj.xpto();
+		Assert.assertEquals(expected, actual);
+	}
+
+}
