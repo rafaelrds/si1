7c08d6d7f0b7099d08452506c4e6cbb959a23940
rafaelrds <crafards@gmail.com>
2013-07-12 16:35:22 -0300

    atualizando paginas e logica

---
+++ lab3/src/tasklist/DateConclusion.java
@@ -0,0 +1,12 @@
+package tasklist;
+
+import java.util.Comparator;
+public class DateConclusion implements Comparator<Task>{
+	@Override
+	public int compare(Task task1, Task task2) {
+		if(task1.getDataConclusao().equals(task2.getDataConclusao())){
+			return task1.getNome().compareTo(task2.getNome());
+		}
+		return task1.getDataConclusao().compareTo(task2.getDataConclusao());
+	}
+}
